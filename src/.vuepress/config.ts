ba9099c46d1c7059033471838d8dfa858a996c4d
Mr.Hope <zhangbowang1998@gmail.com>
2022-03-01 10:51:24 +0800

    chore: update deps

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,5 @@
 import { defineHopeConfig } from "vuepress-theme-hope";
+import themeConfig from "./themeConfig";
 
 export default defineHopeConfig({
   dest: "./dist",
@@ -21,105 +22,5 @@ export default defineHopeConfig({
     },
   },
 
-  theme: "hope",
-
-  themeConfig: {
-    hostname: "https://hope-studio.innenu.com",
-
-    author: {
-      name: "Mr.Hope",
-      url: "https://mrhope.site",
-    },
-
-    iconPrefix: "iconfont icon-",
-
-    logo: "/logo.png",
-
-    repo: "https://github.com/Hope-Studio/Hope-Studio.github.io",
-    docsDir: "src",
-
-    navbar: [
-      "/",
-      "/about",
-      {
-        text: "招新",
-        icon: "profile",
-        prefix: "/invite/",
-        children: ["", "content", "need", "notice"],
-      },
-    ],
-
-    sidebar: {
-      "/": [
-        "",
-        "about",
-        "resource/",
-        {
-          text: "工作室招新",
-          icon: "profile",
-          prefix: "invite/",
-          collapsable: false,
-          children: ["", "content", "need", "notice"],
-        },
-      ],
-    },
-
-    copyright: "Copyright © 2017-present Hope Studio",
-
-    displayFooter: true,
-
-    plugins: {
-      comment: {
-        type: "waline",
-        serverURL: "https://hope-studio-comment.vercel.app",
-      },
-
-      mdEnhance: {
-        presentation: true,
-      },
-
-      pwa: {
-        favicon: "/favicon.ico",
-        themeColor: "#5c92d1",
-        apple: {
-          statusBarColor: "white",
-          icon: "/assets/icon/apple-icon-152.png",
-        },
-        msTile: {
-          image: "/assets/icon/ms-icon-144.png",
-          color: "#ffffff",
-        },
-        manifest: {
-          icons: [
-            {
-              src: "/assets/icon/chrome-192.png",
-              sizes: "192x192",
-              type: "image/png",
-            },
-            {
-              src: "/assets/icon/chrome-512.png",
-              sizes: "512x512",
-              type: "image/png",
-            },
-            {
-              src: "/assets/icon/chrome-mask-192.png",
-              sizes: "192x192",
-              purpose: "maskable",
-              type: "image/png",
-            },
-            {
-              src: "/assets/icon/chrome-mask-512.png",
-              sizes: "512x512",
-              purpose: "maskable",
-              type: "image/png",
-            },
-          ],
-          shortcuts: [
-            { name: "关于工作室", url: "/about.html" },
-            { name: "纳新说明", url: "/invite/need/" },
-          ],
-        },
-      },
-    },
-  },
+  themeConfig,
 });
